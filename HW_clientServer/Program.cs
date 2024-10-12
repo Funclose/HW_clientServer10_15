c6ccdfb85f1668d173f28b48091f7d4fa29b4151
Funclose <ar.doroxoff@gmail.com>
2024-10-12 17:51:57 +0300

    Add project files.

---
+++ HW_clientServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace HW_clientServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectToServer();
+            Console.ReadLine();
+        }
+        static void ConnectToServer()
+        {
+            string connect = "127.0.0.1";
+            int port = 4444;
+            try
+            {
+                byte[] data = new byte[255];
+                TcpClient client = new TcpClient(connect, port);
+
+                NetworkStream stream = client.GetStream();
+                Console.WriteLine("connected to server");
+                StringBuilder reasponData = new StringBuilder();
+                int bytes;
+                Console.WriteLine("hello from client");
+                do
+                {
+                    bytes = stream.Read(data, 0, data.Length);
+                    Task.Delay(1000);
+                    reasponData.Append($"Time now: {DateTime.Now}");
+                }
+                while (stream.DataAvailable);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.Common;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+     class Program
+    {
+        static async Task Main(string[] args)
+        {
+            StartServer();
+        }
+
+        public static void StartServer()
+        {
+
+            IPAddress ipAddres = IPAddress.Parse("127.0.0.1");
+            int port = 4444;
+            TcpListener tcpListener = new TcpListener(ipAddres, port);
+            try
+            {
+                tcpListener.Start();
+                while (true)
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    Console.WriteLine("новый клиекнт подключен");
+                    NetworkStream stream = client.GetStream();
+                    string currentime = DateTime.Now.ToString();
+                    byte[] data = Encoding.ASCII.GetBytes(currentime);
+                    stream.Write(data, 0, data.Length);
+
+                    Console.WriteLine("время с сервера " + currentime);
+
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ошибка с сервера: " + ex.Message);
+            }
+            finally
+            {
+                tcpListener.Stop();
+            }
+        }
+
+
+
+    }
+}
